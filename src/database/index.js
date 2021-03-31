9bf837ff5093fbfc55f50db162efa5a3f0193c38
Cesar Chávez <cesarchavezcal@gmail.com>
2021-03-30 19:14:52 -0600

    Crud Example
    * CRUD Test
    
    Adding CRUD example to duplicate the services as templates
    
    * CRUD Test
    
    Adding CRUD example to duplicate the services as templates
    
    Co-authored-by: Cesar Chavez <cesar.chavez-v@aceable.com>

--- src/database/index.js
+++ src/database/index.js
@@ -18,6 +18,7 @@ module.exports = async () => {
             useCreateIndex: true
         });
     } catch (error) {
+        // eslint-disable-next-line no-console
         console.log(error);
         process.exit(1);
     }
